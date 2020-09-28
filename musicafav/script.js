1d29c51bd49979fa08b257c9093a50b7d4419e35
mari2601 <68756842+mari2601@users.noreply.github.com>
2020-09-28 14:00:07 -0500

    Add files via upload

---
+++ musicafav/script.js
@@ -0,0 +1,60 @@
+function feid() {
+ let name = prompt("¿Quien es ese artista?")   
+ let nombre = document.getElementById("nombre")
+ nombre.textContent = name 
+    
+ let genero = prompt("Cual es su genero musical?")   
+let musical = document.getElementById("genero")
+musical.textContent = genero
+
+let pais = prompt("¿De donde es originario?")   
+let origen = document.getElementById("pais")
+origen.textContent = pais
+
+let cancion = prompt("¿CCual crees que es su cancion mas top?")   
+let top = document.getElementById("cancion")
+    cancion.textContent = top
+
+ 
+}
+function Dom() {
+    let name = prompt("¿Quien es ese artista?")   
+    let nombre = document.getElementById("nombre")
+    nombre.textContent = name 
+       
+    let genero = prompt("Cual es su genero musical?")   
+   let musical = document.getElementById("genero")
+   musical.textContent = genero
+   
+   let pais = prompt("¿De donde es originario?")   
+   let origen = document.getElementById("pais")
+   origen.textContent = pais
+   
+   let cancion = prompt("¿CCual crees que es su cancion mas top?")   
+   let top = document.getElementById("cancion")
+       cancion.textContent = top
+   
+    
+   }
+
+    
+    
+   
+    function capaldi() {
+        let name = prompt("¿Quien es ese artista?")   
+        let nombre = document.getElementById("nombre")
+        nombre.textContent = name 
+           
+        let genero = prompt("Cual es su genero musical?")   
+       let musical = document.getElementById("genero")
+       musical.textContent = genero
+       
+       let pais = prompt("¿De donde es originario?")   
+       let origen = document.getElementById("pais")
+       origen.textContent = pais
+       
+       let cancion = prompt("¿CCual crees que es su cancion mas top?")   
+       let top = document.getElementById("cancion")
+           cancion.textContent = top
+        }
+        
